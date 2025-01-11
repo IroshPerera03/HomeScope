7252638279fe366c93175c59500fd1a12239659d
Irosh Perera <139335410+IroshPerera03@users.noreply.github.com>
2025-01-11 20:24:23 +0530

    Published Succesfully

--- src/App.js
+++ src/App.js
@@ -9,38 +9,30 @@ import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Hero from "./components/hero/hero";
 import About from "./components/about/about";
-import "./App.css";
 import Search from "./components/search/search";
 import PropertyDetails from "./components/propertyDetails/propertyDetails";
-import "react-widgets/styles.css";
 import { FavoritesProvider } from "./context/favoritesContext";
+import "react-widgets/styles.css";
+import "./App.css";
 
 const App = () => {
-  // State to manage dark mode
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // Function to toggle dark mode
   const toggleTheme = () => setIsDarkMode(!isDarkMode);
 
   return (
-    // Provide favorites context to the app
     <FavoritesProvider>
       <Router basename="/HomeScope">
         <div className="app-container">
           <div className={isDarkMode ? "App dark-mode" : "App"}>
-            {/* Header component with theme toggle */}
             <Header toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
             <Routes>
               <Route path="/" element={<Navigate to="/home" replace />} />
-              {/* Route for home page */}
               <Route path="/home" element={<Hero isDarkMode={isDarkMode} />} />
-              {/* Route for search page */}
               <Route path="/search" element={<Search />} />
-              {/* Route for property details page */}
               <Route path="/property/:id" element={<PropertyDetails />} />
               <Route path="/about" element={<About />} />
             </Routes>
-            {/* Footer component */}
             <Footer />
           </div>
         </div>
